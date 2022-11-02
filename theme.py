5dcab5604130e0bed929ff92b8bd4f7cd1f50818
sehilyi <sehilyi@gmail.com>
2022-11-02 09:07:25 -0400

    chore: add detail plot

---
+++ theme.py
@@ -0,0 +1,91 @@
+"""
+Helpers to apply high-level themes to altair charts.
+"""
+def apply_theme(
+    base,
+    title_dy=-10,
+    title_anchor="middle",
+    title_font_size=20,
+    subtitle_font_size=16,
+    axis_title_font_size=16,
+    axis_y_title_font_size=16,
+    axis_label_font_size=16,
+    axis_title_padding=10,
+    axis_tick_color='white',
+    axis_domain_width=2,
+    label_angle=0,
+    legend_orient="right",
+    legend_title_orient="top",
+    legend_stroke_color="transparent",
+    legend_padding=0,
+    legend_symbol_type="circle",
+    legend_title_font_size=16,
+    label_font_size=16,
+    header_label_font_size=18,
+    header_label_orient='top',
+    point_size=70,
+    gradient_length=50,
+    x_label_angle=0,
+    view_stroke_width=1
+):
+    return base.configure(
+        font='Noto Sans',
+    ).configure_header(
+        titleFontSize=16,
+        titleFontWeight=500,
+        labelOrient=header_label_orient,
+        labelFontSize=header_label_font_size,
+        labelFontWeight=500
+    ).configure_title(
+        fontSize=title_font_size,
+        subtitleFontSize=subtitle_font_size,
+        fontWeight=500,
+        anchor=title_anchor,
+        align="left",
+        dy=title_dy,
+        subtitlePadding=5
+    ).configure_axis(
+        # domainWidth=2,
+        labelFontSize=axis_label_font_size,
+        labelFontWeight=400,
+        titleFontSize=axis_title_font_size,
+        titleFontWeight=500,
+        labelLimit=1000,
+        titlePadding=axis_title_padding,
+        tickColor=axis_tick_color,
+        domainWidth=axis_domain_width,
+        domainColor='black',
+        # labelAngle=label_angle
+    ).configure_scale(
+        # bandPaddingOuter=0.5
+    ).configure_axisX(
+        labelAngle=x_label_angle
+    ).configure_axisY(
+        titleFontSize=axis_y_title_font_size,
+        domain=False
+    ).configure_legend(
+        titleFontSize=legend_title_font_size,
+        titleFontWeight=500,
+        labelFontSize=label_font_size,
+        labelFontWeight=400,
+        padding=legend_padding,
+        cornerRadius=0,
+        orient=legend_orient,
+        fillColor="white",
+        symbolStrokeWidth=2,
+        strokeColor=legend_stroke_color,
+        # temporally commented out
+        # symbolType=legend_symbol_type,
+        titleOrient=legend_title_orient,
+        # direction='horizontal',
+        gradientLength=gradient_length
+    ).configure_concat(
+        spacing=0
+    ).configure_view(
+        fill='transparent', # '#F5F5F5', # '#F6F6F6', # '#F5F5F5', '#FBFBFB'
+        strokeWidth=view_stroke_width,
+        stroke='white'
+        # strokeWidth=2
+    ).configure_point(
+        size=point_size
+    )
---
+++ visualize-reports.py
@@ -0,0 +1,101 @@
+# %%
+import pandas as pd
+import altair as alt
+import json
+from theme import apply_theme
+# %%
+data = []
+with open('./a11y-reports.json', 'r') as f:
+    reports = json.load(f)
+    for report in reports:
+        categories = report['report']['categories']
+
+        row = {}
+        row['dbId'] = report['dbId']
+        row['shortName'] = report['shortName']
+        row['url'] = report['url']
+        row['error'] = categories['error']['count']
+        row['contrast'] = categories['contrast']['count']
+        row['alert'] = categories['alert']['count']
+        data.append(row)
+data = pd.DataFrame.from_records(data)
+data.head()
+# %%
+"""
+Overview
+"""
+metric = [
+    ('errors', 'error', '#CC7DAA'),
+    ('contrast issues', 'contrast', '#D6641E'),
+    ('alerts', 'alert', '#E6A01B'),
+]
+
+for (name, field, hex) in metric:
+    plot_alert = alt.Chart(data).mark_bar().encode(
+        x=alt.X(f'{field}:Q', scale=alt.Scale(type='log'), title=f'The number of {name}'),
+        y=alt.Y('shortName:N', sort='-x', title='Data portals'),
+        color=alt.value(hex)
+    ).transform_filter(
+        alt.datum[field] > 0  
+    ).properties(
+        title=f'The Number of "{name.capitalize()}" By Data Portals'
+    )
+
+    plot_alert = apply_theme(plot_alert)
+    # plot_alert.save('report-error.png') # does not work
+    plot_alert.display()
+# %%
+"""
+Detailed issues
+"""
+details = []
+with open('./a11y-reports.json', 'r') as f:
+    reports = json.load(f)
+    for report in reports:
+        metrics = ['error', 'contrast', 'alert']
+        for m in metrics:
+            stats = report['report']['categories'][m]['items']
+
+            row = {}
+            row['dbId'] = report['dbId']
+            row['shortName'] = report['shortName']
+            row['url'] = report['url']
+            row['type'] = m
+
+            for e in stats:
+                name = stats[e]['id']
+                count = stats[e]['count']
+                row_copy = row.copy()
+                row_copy['name'] = name
+                row_copy['count'] = count
+                
+                details.append(row_copy)
+
+details = pd.DataFrame.from_records(details)
+details
+# %%
+plot = alt.Chart(details).mark_rect(
+    stroke='white',
+    opacity=1
+).encode(
+    # x=alt.X('count', scale=alt.Scale(type='linear'), title='The number of issues'),
+    x=alt.X('shortName:N', sort='-color', axis=alt.Axis(labelAngle=270, grid=True, zindex=0)),
+    y=alt.Y('name:N', title=None, axis=alt.Axis(grid=True, zindex=0)),
+    color=alt.Color(
+        'count:Q',
+        scale=alt.Scale(type='log', domain=[1, 100], clamp=True),   
+        legend=alt.Legend(gradientLength=300)
+    ),
+    row=alt.Row('type:N', header=alt.Header(labelOrient='left', title=None))
+    # row=alt.Row('name:N', title=None, header=alt.Header(
+    #     labelOrient='left', labelAngle=0, labelAnchor='middle', labelAlign='left'
+    # ), spacing=0)
+).transform_filter(
+    alt.datum['count'] > 0
+).resolve_scale(y='independent').properties(
+    # height=40,
+    # width=600
+)
+plot = apply_theme(plot)
+plot
+# %%
